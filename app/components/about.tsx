2588e873263fa685124a62e5c6f31b85865af19e
Balazs Doktor <balazs.doktor@sanofi.com>
2025-10-03 14:45:25 +0200

    Add image

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,5 +1,16 @@
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
-import { Link, Element } from "react-scroll";
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  HStack,
+  Image,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { Element } from "react-scroll";
 
 export default function About() {
   return (
@@ -13,10 +24,27 @@ export default function About() {
         justifyContent="center"
         flexDirection="column"
       >
-        <Heading>Holló Villany</Heading>
-        <Text>
-          Megbízható villanyszerelés, gyors kiszállás és precíz munka.
-        </Text>
+        <SimpleGrid columns={{ base: 1, md: 2 }}>
+          <GridItem
+            display="flex"
+            justifyContent={"center"}
+            alignItems="center"
+          >
+            <Flex flexDirection="column" justify="center" align="center">
+              <Heading>Holló Villany</Heading>
+              <Text>
+                Megbízható villanyszerelés, gyors kiszállás és precíz munka.
+              </Text>
+            </Flex>
+          </GridItem>
+          <GridItem
+            display="flex"
+            justifyContent={"center"}
+            alignItems="center"
+          >
+            <Image background="transparent" src="profile.jpg" w="10em"></Image>
+          </GridItem>
+        </SimpleGrid>
       </Box>
     </Element>
   );
