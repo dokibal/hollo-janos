0e4a04ab992852bc87480f53a24e722e83b82732
Balazs Doktor <balazs.doktor@sanofi.com>
2025-10-16 17:51:24 +0200

    Fix use client issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-// TODO: put back "use client";
+"use client";
 
 import { Box } from "@chakra-ui/react";
 import Services from "./modules/services";
@@ -6,15 +6,6 @@ import Contact from "./modules/contact";
 import About from "./modules/about";
 import Header from "./modules/header";
 import Footer from "./modules/footer";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Fejlesztés alatt",
-  robots: {
-    index: false,
-    follow: false,
-  },
-};
 
 export default function Page() {
   return (
